28bd0b93444acbcf7263f51bf03c3ed63d807ae3
Seung-min <peppyksm@gmail.com>
2026-05-06 13:57:00 +0900

    자바 시작, gitignore 추가

---
+++ src/be_study_8/start/HelloWorld.java
@@ -0,0 +1,16 @@
+package be_study_8.start;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, wrold!!");
+		
+//		Run 단축키 Ctrl + F11
+		/*여러줄 주석 
+		 * 줄 2
+		 * 줄 3*/
+		
+//		sysout + Ctrl + Space  시스아웃라인 단축
+	}
+}
