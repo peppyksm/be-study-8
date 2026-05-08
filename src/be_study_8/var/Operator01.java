6886a7a001ef8362e420f976bf95a27a8ff1f06e
Seung-min <peppyksm@gmail.com>
2026-05-08 09:24:05 +0900

    오류해결

---
+++ src/be_study_8/var/Operator01.java
@@ -0,0 +1,24 @@
+package be_study_8.var;
+
+public class Operator01 {
+
+	public static void main(String[] args) {
+		
+		//산술  + - * / %
+		
+		System.out.println( 1+1 );
+		System.out.println( 10-5 );
+		System.out.println( 5*9 );
+		System.out.println( 30/7 );
+		System.out.println( (double)30 / 7);
+		System.out.println( 30/7.0 );
+		System.out.println( 9%5 );
+		System.out.println( 28%9 );
+		
+		//부호 + -
+		System.out.println( 10);
+		System.out.println( +10 );
+		System.out.println( -10 );
+		System.out.println( -(-10));
+	}
+}
