3813507c45e2ad844be1008785e61f943e4316af
Seung-min <peppyksm@gmail.com>
2026-05-15 09:40:30 +0900

    Java quiz16

---
+++ src/be_study_8/quiz/Quiz16.java
@@ -0,0 +1,28 @@
+package be_study_8.quiz;
+
+public class Quiz16 {
+
+	public static void main(String[] args) {
+		
+		
+		int[][] arr = new int[5][5];
+
+		for(int i = 0; i<=4; i++) {
+			
+			for(int j = 0; j<=4; j++) {
+				
+				if(i == j) {
+					arr[i][j] = 1;
+				}
+				else if(i<j) {
+					arr[i][j] = 2;
+				}
+				else {
+					arr[i][j] = 3;
+				}
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}	
+	}
+}
