9d661c67e2ad591e8d5a6f0501e7bd7cf530df5a
Seung-min <peppyksm@gmail.com>
2026-07-29 09:32:03 +0900

    퀴즈 51

--- src/be_study_8/quiz/quiz51/Quiz51.java
+++ src/be_study_8/quiz/quiz51/Quiz51.java
@@ -5,6 +5,8 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+
+
 public class Quiz51 {
 
 	public static void main(String[] args) {
@@ -60,18 +62,68 @@ public class Quiz51 {
 			
 			JSONObject jsonObj = (JSONObject)parser.parse(jsonString);
 			
+			
 			JSONArray employees = (JSONArray)jsonObj.get("employees");
 			
 			for(int i=0; i<employees.size(); i++) {
-				JSONObject employee = (JSONObject)employees.get(i);
+				JSONObject emp = (JSONObject)employees.get(i);
+				System.out.print("ID:" +emp.get("id") + " ");
+				System.out.print("이름:" + emp.get("name") + " ");
+				System.out.print("포지션:" + emp.get("position") + " ");
+				System.out.print("연봉:" + emp.get("salary") + " ");
 				
-				System.out.println("-----employee----" + i);
-				System.out.print("아이디 : " +employee.get("id"));
-				System.out.print("이름 : " + employee.get("name"));
-				System.out.print("포지션 :  " + employee.get("position"));
-				System.out.print("연봉 : " + employee.get("salary"));
-				System.out.println();	
-			}		
+				JSONArray skills = (JSONArray)emp.get("skills");
+				
+				System.out.print("보유 스킬:");
+				
+				for(int j = 0 ; j < skills.size(); j++) {
+					System.out.print(skills.get(j) + " ");
+				}
+				System.out.println();
+			}	
+			
+			
+			
+			
+			JSONObject company = (JSONObject)jsonObj.get("company");
+			
+			System.out.print(company.get("name") + " ");
+			System.out.print(company.get("address") + " ");
+			System.out.println(company.get("established"));
+			
+			JSONArray departments = (JSONArray)company.get("departments");
+			
+			for(int i = 0; i < departments.size(); i++) {
+				JSONObject dept = (JSONObject)departments.get(i);
+				
+				System.out.print(dept.get("name") + " ");
+				JSONArray emps = (JSONArray)dept.get("employees");
+				for(int j = 0; j < emps.size(); j++) {
+					System.out.print(emps.get(j) + "번 사원 ");
+				}
+				System.out.println();
+			}
+			
+			
+			
+			
+			
+			JSONArray projects = (JSONArray)jsonObj.get("projects");
+			
+			for(int i = 0; i < projects.size(); i++) {
+				JSONObject pro = (JSONObject)projects.get(i);
+				
+				System.out.print("프로젝트:" + pro.get("title") + " ");
+				System.out.print("예산:" + pro.get("budget") + " ");
+				JSONArray team = (JSONArray)pro.get("team");
+				System.out.print("소속 인원:");
+				for(int j = 0; j < team.size(); j++) {
+					System.out.print(team.get(j) + "번 사원 ");
+				}
+				System.out.println();
+			}
+			
+			
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
